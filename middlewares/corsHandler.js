312af2356f5cc5585b51cf61ce7c37918b8ae8f4
Anton Zemskov <antoshkow1703@gmail.com>
2023-06-25 22:27:04 +0300

    debug cors

--- middlewares/corsHandler.js
+++ middlewares/corsHandler.js
@@ -6,9 +6,12 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
 
   if (ALLOWED_CORS.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', ALLOWED_CORS);
+    console.log('if');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
   }
+
+  console.log(origin, ALLOWED_CORS);
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
