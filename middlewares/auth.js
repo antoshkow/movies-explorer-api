d0fc9386e51b4de4c64c1e1b436b53f438b60f6a
Anton Zemskov <antoshkow1703@gmail.com>
2023-07-08 10:44:46 +0300

    change cookie to headers

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,16 +3,25 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_MODE } = require('../utils/config');
 
 module.exports = (req, res, next) => {
-  const { jwt: token } = req.cookies;
+  // const { jwt: token } = req.cookies;
 
-  if (!token) {
+  // if (!token) {
+  //   throw new UnauthorizedError('Необходима авторизация');
+  // }
+
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Необходима авторизация');
   }
+
+  const bearerToken = authorization.replace('Bearer ', '');
 
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_MODE);
+    // payload = jwt.verify(token, JWT_MODE);
+    payload = jwt.verify(bearerToken, JWT_MODE);
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
